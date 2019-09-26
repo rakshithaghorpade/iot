264c10025643a4cef8b0df5e785b86e0d00d3ad7
rakshithaghorpade <53851629+rakshithaghorpade@users.noreply.github.com>
2019-09-26 11:52:40 +0530

    Update ultrasonic.ino

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,65 +1,35 @@
-int CT=30;
-long unsigned int lowin;
-long unsigned int pause=5000;
-boolean locklow=true;
-boolean takelowtime;
-int pirpin=4;
-int ledpin=13;
-void setup()
-{
-  Serial.begin(9600);
- pinMode(ledpin,OUTPUT);
-pinMode(pirpin,INPUT); 
-digitalWrite(pirpin,LOW);
-Serial.print("Calibrating the sensor");
-for(int i=0;i<CT;i++)
-{
-  Serial.print('.');
-  delay(100);
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(12,OUTPUT);
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
 }
-Serial.println("DONE");
-Serial.println("sensor active");
-delay(10);
-}
-void loop()
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+if(distance<10)
 {
-     if(digitalRead(pirpin)==HIGH)
-     {
-      digitalWrite(ledpin,HIGH);
-        if(locklow)
-        {
-          locklow=false;
-          Serial.println("___");
-          Serial.println("Motion detected at");
-          Serial.println((millis()/1000));
-          Serial.println("Sec");
-          delay(50);
-        }
-        takelowtime=true;
-      }
-      if(digitalRead(pirpin)==LOW)
-      {
-          digitalWrite(ledpin,LOW);
-          if(takelowtime)
-          {
-            lowin=millis();
-            takelowtime=false;
-          }
-          if(!locklow&&millis()-lowin>pause)
-          {
-            locklow=true;
-         
-          Serial.println("Motion detected at");
-          Serial.println((millis()-pause)/1000);
-          Serial.println("Sec");
-          delay(50);
-          }
-      }
-     }
-  
-  
-  
-
-    
-
-  
+  digitalWrite(12,HIGH);
+}
+else
+{
+  digitalWrite(12,LOW);
+}
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+}
